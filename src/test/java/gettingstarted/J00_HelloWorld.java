b5520766e5add52187bade612fe135afcc3b8d71
sathishalthus-learning <sathishalthus.learning@gmail.com>
2024-12-16 22:08:29 +0530

    00-Getting Started

--- src/test/java/gettingstarted/J00_HelloWorld.java
+++ src/test/java/gettingstarted/J00_HelloWorld.java
@@ -1,4 +1,4 @@
-package testclasses;
+package gettingstarted;
 
 public class J00_HelloWorld {
 	//
---
+++ src/test/java/javadocs/J00_HelloWorld.java
@@ -0,0 +1,8 @@
+package javadocs;
+
+public class J00_HelloWorld {
+	public static void main(String arg[]) {
+		System.out.println("Hello Java !");
+	}
+
+}
